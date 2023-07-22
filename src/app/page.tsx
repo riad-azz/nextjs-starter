6aeecc5037127089ab1bba866033096b8881fa2c
Riadh <riad.azz.mail@gmail.com>
2023-07-22 11:29:59 +0100

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ThemeButton from "@/components/ThemeButton";
 
 export default async function Home() {
   return (
-    <main className="flex flex-col gap-4 dark:bg-red-600 p-4">
+    <main className="flex flex-col gap-4 p-4 dark:bg-red-600">
       <h1 className="text-3xl md:text-4xl">Hello Next.js Template!!</h1>
       <ThemeButton />
     </main>
